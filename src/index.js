dc3d605ec570a80cf0041361e138b5fb53f12034
Igor Drozdov <igor_drozdov@epam.com>
2017-09-27 21:22:11 +0300

    Game view

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Buildings = ({heights, height}) => <g>
+    {
+        heights.map((h, i) => <rect x={i} y={height - h} width={1} height={h}/>)
+    }
+</g>;
+
+const GameView = ({heights, position, bomb, width, height}) => <svg viewBox={`0 0 ${width} ${height}`}>
+    <Buildings heights={heights} height={height}/>
+    <circle cx={position.x - .5} cy={position.y - .5} r={.5}/>
+</svg>;
+
+ReactDOM.render(<GameView heights={Array(50).fill().map(i => Math.random() * 18 | 0)}
+                          width={80}
+                          height={24}
+                          position={{x: 3, y: 1}}
+    />,
+    document.getElementById('root'));
